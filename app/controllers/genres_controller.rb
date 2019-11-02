bfda048d3c477e8d70d5680b49e2d8d5cfaae388
missouza96 <missouza96@gmail.com>
2019-11-02 20:33:06 -0300

    add genre_controller

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,28 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.filtrar(params['name'])
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to @genre
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
