31c850fba0e65e1a1706f4a644298cae2ca0bbbc
Élisson Michael <elissonmichael@gmail.com>
2019-11-06 19:42:27 -0300

    Fix Routing Error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists 
-  resources :tracks, except :destroy
-  resources :albums, except :destroy
+  resources :artists
+  resources :tracks, except: :destroy
+  resources :albums, except: :destroy
   root 'artists#index'
 end
