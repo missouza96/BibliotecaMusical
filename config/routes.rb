67e275c630911ad7993720248330fc6058ba22c4
Edgar Salardani <42452263+edgarssenhorello@users.noreply.github.com>
2019-11-06 21:56:53 -0300

    Rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artists 
-  resources :tracks, except :destroy
-  resources :albums, except :destroy
+  resources :tracks
+  resources :albums
   root 'artists#index'
 end
