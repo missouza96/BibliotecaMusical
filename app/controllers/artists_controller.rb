18771b1d09415466179a3b88c0e5822793ff0c98
missouza96 <missouza96@gmail.com>
2019-11-10 15:04:59 -0300

    CRUD artistas
    #autor: milena

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,11 @@
 class ArtistsController < ApplicationController
 	def index
-		@artists = Artist.order(:name) #mostra todos
+		@artists = Artist.order(:name) 
 	end
 
 	def show
-		@artist = Artist.find(params[:id]) #mostra cada artista
+		@artist = Artist.find(params[:id]) 
 	end
-<<<<<<< Updated upstream
 	def edit
 		@artist = Artist.find(params[:id])		
 	end
@@ -20,13 +19,10 @@ class ArtistsController < ApplicationController
 		end
 	end
 
-	private 
-	def artist_params
-		params.require(:artist).permit(:name)
-=======
 	def new
 		@artist = Artist.new
 	end
+	
 	def create
 		@artist = Artist.new(artist_params)
 		if @artist.save
@@ -34,8 +30,12 @@ class ArtistsController < ApplicationController
 		else
 			render :new	
 		end
->>>>>>> Stashed changes
+
 	end
 
+	private 
+	def artist_params
+		params.require(:artist).permit(:name)
+	end
 
 end
