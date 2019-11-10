22fbd7f2e65cac89eaae37a35155c1cfd9331f5f
missouza96 <missouza96@gmail.com>
2019-11-10 14:53:19 -0300

    CRUD artistas
    #autor: milena

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,12 +4,38 @@ class ArtistsController < ApplicationController
 	end
 
 	def show
-		@artists = Artist.find(params[:id]) #mostra cada artista
+		@artist = Artist.find(params[:id]) #mostra cada artista
 	end
-	def delete
-		
+<<<<<<< Updated upstream
+	def edit
+		@artist = Artist.find(params[:id])		
 	end
 	def update
+		@artist = Artist.find(params[:id])
 
+		if @artist.update_attributes(artist_params)
+			redirect_to @artist
+		else
+			render :edit
+		end
 	end
+
+	private 
+	def artist_params
+		params.require(:artist).permit(:name)
+=======
+	def new
+		@artist = Artist.new
+	end
+	def create
+		@artist = Artist.new(artist_params)
+		if @artist.save
+			redirect_to @artist
+		else
+			render :new	
+		end
+>>>>>>> Stashed changes
+	end
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def formatar(data)
+    data.strftime("%m/%d/%Y %I:%M%p")
+  end
 end
