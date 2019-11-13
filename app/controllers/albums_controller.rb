b32b21ed32e480549d9162d95216d63afc07141c
Edgar Salardani <42452263+edgarssenhorello@users.noreply.github.com>
2019-11-13 19:42:58 -0300

    CRUD Albums
    Autor: Edgar Salardani

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,29 @@
 class AlbumsController < ApplicationController
     def index
-		@albums = Album.order(:tracks)
+		@albums = Album.order(:title) 
+	end
+
+	def show
+		@album = Album.find(params[:id])
+	end
+
+	def edit
+		@album = Album.find(params[:id])		
+	end
+
+	def update
+		@album = Album.find(params[:id])
+
+		if @album.update_attributes(album_params)
+			redirect_to @album
+		else
+			render :edit
+		end
+	end
+
+
+	private 
+	def album_params
+		params.require(:album).permit(:title)
 	end
 end
