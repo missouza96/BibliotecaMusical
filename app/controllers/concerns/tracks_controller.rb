ca4a8898056b0626b0b57ad06b2773204db9681b
Edgar Salardani <42452263+edgarssenhorello@users.noreply.github.com>
2019-11-15 07:39:48 -0300

    Tracks Controller e Show

---
+++ app/controllers/concerns/tracks_controller.rb
@@ -0,0 +1,54 @@
+class TracksController < ApplicationController
+
+  def index
+    @tracks = Track.order(:name)
+  end
+
+  def show
+    @track = Track.find(params[:id])
+  end
+
+  def new
+    @track = Track.new
+    @albums = Album.all
+    @genres = Genre.all
+    @mediatypes = MediaType.all
+  end
+
+  def create
+    @albums = Album.all
+    @track = Track.new(track_params)
+    @mediatypes = MediaType.all
+    @genres = Genre.all
+    if @track.save
+      redirect_to @track
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @albums = Album.all
+    @track = Track.find(params[:id])
+    @mediatypes = MediaType.all
+    @genres = Genre.all
+  end
+
+  def update
+    @albums = Album.all
+    @track = Track.find(params[:id])
+    @mediatypes = MediaType.all
+    @genres = Genre.all
+    if @track.update_attributes(track_params)
+       redirect_to @track
+    else
+       render :edit
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:name, :album_id, :media_type_id, :genre_id, :composer, :milliseconds, :bytes, :unit_price)
+  end
+end
